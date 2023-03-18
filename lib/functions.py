5cb75318fdbf21e97d04dc9b5c970d54b8e2956b
Vanessa <vanessa.co403@gmail.com>
2023-03-18 11:15:24 -0400

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name ="vanessa"):
+    print(f"Hello, {name}!")
+
+greet()
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+  
 
 def halve(number):
-    pass
+    return number / 2
